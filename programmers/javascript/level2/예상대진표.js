84c6cec2697488f33c63c208c2abc7d112203261
Kyun Heo <kyun2dot@gmail.com>
2020-06-29 23:16:21 +0900

    feat : programmers level2 with js

---
+++ programmers/javascript/level2/예상대진표.js
@@ -0,0 +1,15 @@
+const n = 8;
+const a = 4;
+const b = 7;
+
+const solution = (n, a, b) => {
+  let answer = 0;
+  while (a != b) {
+    a = Math.ceil(a / 2);
+    b = Math.ceil(b / 2);
+    answer++;
+  }
+  return answer;
+};
+
+console.log(solution(n, a, b));
