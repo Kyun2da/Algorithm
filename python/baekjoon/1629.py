e32b8303210d44974e7dc829bb94b9c7168454c3
Kyun Heo <kyun2dot@gmail.com>
2020-08-25 15:37:38 +0900

    feat: 곱셈

---
+++ python/baekjoon/1629.py
@@ -0,0 +1,15 @@
+def mul(a, b):
+    if b == 0:
+        return 1
+    elif b == 1:
+        return a
+    elif b % 2 > 0:
+        return mul(a, b - 1) * a
+    else:
+        d = mul(a, b // 2)
+        d %= c
+        return d ** 2 % c
+
+a, b, c = map(int, input().split())
+
+print(mul(a, b) % c)
