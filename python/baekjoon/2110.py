477f93de2a79e4c81bb2f100c4136a45f5f8b989
Kyun Heo <kyun2dot@gmail.com>
2020-08-29 22:30:24 +0900

    feat: 공유기 설치

---
+++ python/baekjoon/2110.py
@@ -0,0 +1,28 @@
+import sys
+
+n, c = map(int, input().split())
+
+arr = []
+for _ in range(n):
+    arr.append(int(sys.stdin.readline()))
+
+arr.sort()
+
+start = arr[1] - arr[0]
+end = arr[-1] - arr[0]
+
+maxVal = 0
+while start <= end:
+    mid = (start + end) // 2
+    idx, result = 0, 1
+    for i in range(1, len(arr)):
+        if arr[idx] + mid <= arr[i]:
+            result += 1
+            idx = i
+    if result < c:
+        end = mid - 1
+    elif result >= c:
+        maxVal = mid
+        start = mid + 1
+
+print(maxVal)
