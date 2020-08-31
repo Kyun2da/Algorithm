4ab7feb78236ee0ed982e7ac91d89687a799adf1
Kyun Heo <kyun2dot@gmail.com>
2020-08-31 17:40:54 +0900

    feat: 최소 힙

---
+++ python/baekjoon/1927.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+
+heap = []
+
+for _ in range(n):
+    k = int(sys.stdin.readline())
+    if k ==0:
+        if heap:
+            print(heap[0])
+            heapq.heappop(heap)
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap,k)
+
