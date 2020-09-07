2dca36f81c9d5b8c9228d9e7249f2070e2acabfd
Kyun Heo <kyun2dot@gmail.com>
2020-09-07 16:44:02 +0900

    feat: graph 개념

---
+++ python/thisIsCodingTest/미래도시.py
@@ -0,0 +1,29 @@
+INF = float('inf')
+
+n, m = map(int, input().split())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if i == j:
+            graph[i][j] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+x, k = map(int, input().split())
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+distance = graph[1][k] + graph[k][x]
+
+if distance >= INF:
+    print("-1")
+else:
+    print(distance)
