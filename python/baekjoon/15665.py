cba78a21dfcb17b0c1db3353685577354dc4d9c9
Kyun Heo <kyun2dot@gmail.com>
2020-09-08 20:24:01 +0900

    N과M (11)

---
+++ python/baekjoon/15665.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+def dfs(vis):
+    if len(vis) == m:
+        print(' '.join(map(str, vis)))
+        return
+    overlap = 0
+    for i in range(n):
+        if overlap == arr[i]: continue
+        overlap = arr[i]
+        vis.append(arr[i])
+        dfs(vis)
+        vis.pop()
+
+
+dfs([])
