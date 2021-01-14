bc4a3e7a902c9810ca579ebbb51d408db3634d93
Kyun Heo <kyun2dot@gmail.com>
2021-01-14 09:51:55 +0900

    feat: 그리디

---
+++ python/programmers/level2/구명보트.py
@@ -0,0 +1,18 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    i = 0
+    j = len(people) - 1
+    while i < j:
+        if people[i] + people[j] <= limit:
+            i += 1
+            j -= 1
+        else:
+            j -= 1
+        answer += 1
+    if i == j:
+        answer += 1
+    return answer
+
+
+print(solution([10, 20, 30, 40, 50, 60, 70, 80, 90], 100))
