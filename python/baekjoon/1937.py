bb71ef9e2f2d71316eafeaf4363594112434e780
Kyun Heo <kyun2dot@gmail.com>
2021-04-04 12:55:39 +0900

    feat : dfs

--- python/baekjoon/1937.py
+++ python/baekjoon/1937.py
@@ -1,9 +1,11 @@
 import sys
+
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10 ** 6)
+
 
 def dfs(x, y):
-    if dp[x][y]: return dp[x][y]
+    if dp[x][y]: return dp[x][y] # 이미 한번 왔다간 경로는 그대로 리턴
     dp[x][y] = 1
     for i in range(4):
         nx = x + dx[i]
@@ -26,4 +28,5 @@ for i in range(n):
     for j in range(n):
         answer = max(answer, dfs(i, j))
 
+print(dp)
 print(answer)
