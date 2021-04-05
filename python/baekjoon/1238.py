a7e3b85c4fb10e6d47626750bec25618a0b2bb11
Kyun Heo <kyun2dot@gmail.com>
2021-04-05 23:06:33 +0900

    feat : dijkstra

---
+++ python/baekjoon/1238.py
@@ -0,0 +1,44 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+INF = 1e9
+
+n, m, x = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b, t = map(int, input().split())
+    graph[a].append((b, t))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+tmp_arr = [[0] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    distance = [INF] * (n + 1)
+    dijkstra(i)
+    for j in range(1, n + 1):
+        tmp_arr[i][j] = distance[j]
+
+answer = 0
+
+for i in range(1, n + 1):
+    answer = max(answer, tmp_arr[i][x] + tmp_arr[x][i])
+
+print(answer)
