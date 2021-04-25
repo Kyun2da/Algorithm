a541af1337dcaac912f8ecee725989ac282210ce
Kyun Heo <kyun2dot@gmail.com>
2021-04-25 13:29:44 +0900

    fix : stack

--- python/baekjoon/2504.py
+++ python/baekjoon/2504.py
@@ -1,18 +1,21 @@
+import sys
+
 s = input()
 
 stack = []
 
 answer = 0
 for x in s:
-    if x == '(':
-        stack.append('(')
-    elif x == ')':
+    if x == ')':
         tmp = 0
+        if len(stack) == 0:
+            print(0)
+            sys.exit(0)
         while len(stack) != 0:
             top = stack.pop()
             if top == '[':
                 print(0)
-                exit()
+                sys.exit(0)
             elif top == '(':
                 if tmp == 0:
                     stack.append(2)
@@ -21,15 +24,16 @@ for x in s:
                 break
             else:
                 tmp += top
-    elif x == '[':
-        stack.append('[')
-    else:
+    elif x == ']':
         tmp = 0
+        if len(stack) == 0:
+            print(0)
+            sys.exit(0)
         while len(stack) != 0:
             top = stack.pop()
             if top == '(':
                 print(0)
-                exit()
+                sys.exit(0)
             elif top == '[':
                 if tmp == 0:
                     stack.append(3)
@@ -38,12 +42,17 @@ for x in s:
                 break
             else:
                 tmp += top
+    elif x == '(' or x == '[':
+        stack.append(x)
+    else:
+        print(0)
+        sys.exit(0)
     # print(stack)
 
 for i in stack:
-    if i == '(' or i == ')':
+    if i == '(' or i == '[':
         print(0)
-        exit()
+        sys.exit(0)
     else:
         answer += i
 
