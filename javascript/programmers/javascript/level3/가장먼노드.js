a9e665053300b30e5c131fd898928d3854ef1b59
Kyun Heo <kyun2dot@gmail.com>
2021-05-26 16:53:25 +0900

    update : 시간초과 코드 업데이트

--- javascript/programmers/javascript/level3/가장먼노드.js
+++ javascript/programmers/javascript/level3/가장먼노드.js
@@ -12,32 +12,31 @@ const edge = [
 const solution = (n, edge) => {
   const visited = new Array(n + 1).fill(-1);
   const queue = [1];
+  const adjList = new Array(n + 1).fill(null).map(() => Array());
   visited[0] = 0;
   visited[1] = 0;
+  for (let i of edge) {
+    adjList[i[0]].push(i[1]);
+    adjList[i[1]].push(i[0]);
+  }
   //모든 노드를 방문할때까지 계속한다.
-  while (edge.length !== 0) {
+  while (queue.length > 0) {
     //방문한 노드를 큐에서 뺀다.
-    const Node = queue.shift();
+    const node = queue.shift();
     //edge를 순회하며 다음 방문할 노드를 찾는다.
-    for (let i = 0; i < edge.length; i++) {
-      //방문할 노드가 있다면
-      if (edge[i].includes(Node)) {
-        const filter = edge[i].filter((e) => e !== Node);
+    //방문할 노드가 있다면
+    if (adjList[node]) {
+      adjList[node].forEach((n) => {
         //이미 방문했던 노드라면 엣지를 그냥 skip한다.
-        //console.log(filter);
-        if (visited[filter[0]] !== -1) {
-          edge.splice(i, 1);
-          i--;
-          continue;
+        if (visited[n] === -1) {
+          //방문하지 않았던 노드라면 노드를 방문해준다.
+          queue.push(n);
+          visited[n] = visited[node] + 1;
         }
-        //방문하지 않았던 노드라면 노드를 방문해준다.
-        queue.push(filter[0]);
-        visited[filter[0]] = visited[Node] + 1;
-        edge.splice(i, 1);
-        i--;
-      }
+      });
     }
   }
+  console.log(visited);
   const maxNum = Math.max(...visited);
   return visited.filter((e) => e === maxNum).length;
 };
